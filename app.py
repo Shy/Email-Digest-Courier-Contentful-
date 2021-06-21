1811d69ee168301822f726fef1d4c89030adcd74
Shy Ruparel <Shy@users.noreply.github.com>
2021-06-21 15:02:45 -0400

    Added blogposts

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email
+from contentful import Client
+from flaskext.markdown import Markdown
+import pendulum
 import os
+
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("secret_key")
+Markdown(app)
+
+client = Client(
+    os.environ.get("CTFL_SpaceID"),
+    os.environ.get("CTFL_CDA"),
+    environment="master",
+)
+
+
+@app.template_filter("strftime")
+def _jinja2_filter_datetime(date, fmt="dddd MMMM Do YYYY"):
+    date = pendulum.parse(f"{date.year}-{date.month}-{date.day}", strict=False)
+    return date.format(fmt)
 
 
 class subscribeForm(FlaskForm):
@@ -13,8 +30,17 @@ class subscribeForm(FlaskForm):
 
 
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
+def home():
     form = subscribeForm()
     if request.method == "POST" and form.validate():
         return form.email.data
-    return render_template("index.html", form=form)
+    posts = client.entries({"content_type": "blogPost", "order": "-fields.publishDate"})
+    return render_template("index.html", form=form, posts=posts)
+
+
+@app.route("/post/<string:slug>")
+def post(slug):
+    post = client.entries({"content_type": "blogPost", "fields.slug": slug})
+    if post:
+        return render_template("post.html", post=post[0])
+    return redirect(url_for("home"))
