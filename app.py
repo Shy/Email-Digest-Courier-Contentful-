cd25927095d8a36089d6720b8c2f729cdc57fd8a
Shy Ruparel <Shy@users.noreply.github.com>
2021-06-21 12:20:39 -0400

    Prep Code

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("secret_key")
 
 
-@app.route("/")
+class MyForm(FlaskForm):
+    email = StringField("Your email", validators=[DataRequired(), Email()])
+
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template(
-        "index.html",
-    )
+    form = MyForm()
+    if request.method == "POST" and form.validate():
+        return form.email.data
+    return render_template("index.html", form=form)
